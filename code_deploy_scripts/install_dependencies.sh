2a61f6e45ed9acdf12232873351baf483359132d
guptanirman11 <anshulgupta@Anshuls-MacBook-Air.local>
2024-03-21 17:52:41 -0500

    path

--- code_deploy_scripts/install_dependencies.sh
+++ code_deploy_scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pip3 install -r /home/ec2-user/app/requirements.txt
-cp /home/ec2-user/app/scripts/app.service /etc/systemd/system/
+cp /home/ec2-user/code_deploy_scripts/app.service /etc/systemd/system/
