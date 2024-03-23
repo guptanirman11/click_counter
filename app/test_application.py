8184a04f67528e790188f1e282d543390a7c6e95
guptanirman11 <anshulgupta@Anshuls-MacBook-Air.local>
2024-03-23 01:30:57 -0500

    All test checked again

--- app/test_application.py
+++ app/test_application.py
@@ -1,8 +1,8 @@
 import pytest
 from application import application
 import json
+import time
 
-value = 0
 
 @pytest.fixture
 def client():
@@ -30,3 +30,24 @@ def test_get_counter_route(client):
     data = json.loads(response.data.decode('utf-8'))
     #  checked with the numbers locally but later on changed it to 0 as previous tests clicks everytime so number had to be increases constantly.
     assert data['counter'] != 0
+
+def test_unsupported_method(client):
+    """Test application response to an unsupported HTTP method."""
+    response = client.put('/click')
+    assert response.status_code == 405 
+
+
+def test_multiple_clicks(client):
+    """Test multiple clicks to see if counter increments correctly."""
+    response = client.get('/counter')
+    assert response.status_code == 200
+    data = json.loads(response.data.decode('utf-8'))
+    old_value = data['counter']
+    
+    for _ in range(5):
+        response = client.post('/click')
+        assert response.status_code == 200
+    time.sleep(5)
+    counter_response = client.get('/counter')
+    counter_data = json.loads(counter_response.data.decode('utf-8'))
+    assert counter_data['counter'] >= old_value + 5
