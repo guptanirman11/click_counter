7e77e89a11b96cf14dcb555083f34fc81e709c1a
guptanirman11 <anshulgupta@Anshuls-MacBook-Air.local>
2024-03-24 00:11:29 -0500

    Checking CI/CD

--- app/application.py
+++ app/application.py
@@ -1,78 +1,3 @@
-# from flask import Flask, jsonify, render_template
-# from flask_cors import CORS
-# from worker import PullWorker
-# from db import RedisClient
-# import redis
-# import time
-# from queue import Queue
-# import boto3
-
-# # Connecting with Cloudwatch to log the metrics
-# cloudwatch = boto3.client('cloudwatch', region_name='us-east-1')
-
-# app = Flask(__name__)
-# application = app
-# CORS(application)
-
-# # Initializing Redis connection
-# redis_conn = redis.StrictRedis(host='my-redis-cluster.0diz5e.ng.0001.use1.cache.amazonaws.com', port=6379, db=0, decode_responses=True)
-
-# # Initializing a thread-safe queue for counter increment actions
-# increment_queue = Queue()
-
-# def fetch_db_value():
-#     # fetching value from Redis
-#     value = redis_conn.get('counter') or 0
-#     print(f'fetched: {value}')
-#     return int(value)
-
-# """Starting the Worker"""
-# worker = PullWorker(host='my-redis-cluster.0diz5e.ng.0001.use1.cache.amazonaws.com', port=6379, db=0, increment_queue=increment_queue)
-# worker.start()
-
-# '''Rendering the wesite isung rest api endpoint'''
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# '''Post method called everytime a click is made'''
-# @app.route('/click', methods=['POST'])
-# def click():
-#     # Enqueue an increment action
-#     increment_queue.put(1)
-#     print("Click Registered")
-#     # Send custom metric data to CloudWatch
-#     cloudwatch.put_metric_data(
-#         Namespace='MyApplication',
-#         MetricData=[
-#             {
-#                 'MetricName': 'Clicks',
-#                 'Dimensions': [
-#                     {
-#                         'Name': 'Application',
-#                         'Value': 'MyClickCounterApp'
-#                     },
-#                 ],
-#                 'Unit': 'Count/Second',
-#                 'Value': 1
-#             },
-#         ]
-#     )
-#     return jsonify({'message': 'Increment queued'}), 200
-
-# '''Get method to fetch the Click Value aiding in implementing the live updates'''
-# @app.route('/counter', methods=['GET'])
-# def get_counter():
-#     # Return the most up-to-date counter value from Redis
-#     current_counter = fetch_db_value()
-#     return jsonify({'counter': current_counter}), 200
-
-# if __name__ == '__main__':
-#     # Runnign the Flask app
-#     application.run(debug=True, use_reloader=False)
-
-
-# application.py
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 from worker import PullWorker
@@ -80,28 +5,33 @@ from queue import Queue
 import boto3
 from db import RedisClient
 
+# Class for Cloud Clicker Flask App
 class CloudClickerApplication:
     def __init__(self):
         self.app = Flask(__name__)
         CORS(self.app)
 
+        # Redis cache object from db.py
         self.redis_client = RedisClient()
         self.increment_queue = Queue()
 
-        # Setup CloudWatch
+        # CloudWatch Setup
         self.cloudwatch = boto3.client('cloudwatch', region_name='us-east-1')
 
-        # Start worker
+        # Pull worker object from worker.py
         self.worker = PullWorker(host='my-redis-cluster.0diz5e.ng.0001.use1.cache.amazonaws.com', port=6379, db=0, increment_queue=self.increment_queue)
         self.worker.start()
 
         self.setup_routes()
 
+    # Method for API endpoints
     def setup_routes(self):
+        # Renders the home page
         @self.app.route('/')
         def index():
             return render_template('index.html')
 
+        # registers the click event and put in the queue
         @self.app.route('/click', methods=['POST'])
         def click():
             self.increment_queue.put(1)
@@ -111,7 +41,8 @@ class CloudClickerApplication:
                 MetricData=[{'MetricName': 'Clicks', 'Dimensions': [{'Name': 'Application', 'Value': 'MyClickCounterApp'}], 'Unit': 'Count/Second', 'Value': 1}]
             )
             return jsonify({'message': 'Increment queued'}), 200
-
+        
+        # responds to the Get call to fetch the updated value in Cache
         @self.app.route('/counter', methods=['GET'])
         def get_counter():
             current_counter = self.redis_client.fetch_db_value()
@@ -121,5 +52,5 @@ if __name__ == '__main__':
     cloud_clicker_application = CloudClickerApplication()
     cloud_clicker_application.app.run(debug=True, use_reloader=False)
 
-# Expose the Flask application instance for Gunicorn
+# Exposing the Flask application instance for Gunicorn
 application = cloud_clicker_application.app
--- app/db.py
+++ app/db.py
@@ -1,29 +1,16 @@
 import redis
 
-# # Initializing Redis connection
-# def get_redis_connection():
-#     return redis.StrictRedis(host='my-redis-cluster.0diz5e.ng.0001.use1.cache.amazonaws.com', port=6379, db=0, decode_responses=True)
-
-# # Fetch the current value of the counter from Cache
-# def fetch_db_value(redis_conn):
-#     value = redis_conn.get('counter') or 0
-#     print(f'Fetched: {value}')
-#     return int(value)
-
-# # Increment the counter
-# def increment_counter(redis_conn, increment=1):
-#     redis_conn.incrby('counter', increment)
-
-
-
 class RedisClient:
+    # Establishes the connection
     def __init__(self, host='my-redis-cluster.0diz5e.ng.0001.use1.cache.amazonaws.com', port=6379, db=0):
         self.redis_conn = redis.StrictRedis(host=host, port=port, db=db, decode_responses=True)
 
+    # Method to fetch the value
     def fetch_db_value(self):
         value = self.redis_conn.get('counter') or 0
         print(f'Fetched: {value}')
         return int(value)
 
+    # Method to increment the counter
     def increment_counter(self, increment=1):
         self.redis_conn.incrby('counter', increment)
