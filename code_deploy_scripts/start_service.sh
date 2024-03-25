1916e9fd8613ed59e8f7df7d09eee11cffd1e660
guptanirman11 <anshulgupta@Anshuls-MacBook-Air.local>
2024-03-25 12:02:27 -0500

    chnaged log files

--- code_deploy_scripts/start_service.sh
+++ code_deploy_scripts/start_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-systemctl daemon-reload
-systemctl enable app.service
-systemctl start app.service
+sudo systemctl daemon-reload
+sudo systemctl enable app.service
+sudo systemctl start app.service
